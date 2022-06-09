1b7584bdac3f38f68496141a551b33d7626b6a12
Aaron <ajonaaronajon@yahoo.com>
2022-06-08 19:54:25 -0500

    added Icon component with Icon styles

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,11 +1,12 @@
 import React , {useState} from "react";
 import "./Card.css";
+import Icon from "./Icon";
 
 const Card = ()=>{
 
     return (
         <div className="card">
-
+            <Icon />
         </div>
     )
 }
---
+++ src/components/Icon.js
@@ -0,0 +1,13 @@
+import React , {useState} from "react";
+import "./Icon.css";
+
+const Icon = ()=>{
+
+    return (
+        <div className="icon circle blue">
+
+        </div>
+    )
+}
+
+export default Icon;
