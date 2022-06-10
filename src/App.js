61fb22abf0d8c4a556ce3f4a7785ba9ba847f795
Aaron <ajonaaronajon@yahoo.com>
2022-06-09 19:51:59 -0500

    added more shapes to Icon.css and added generateCard function

--- src/App.js
+++ src/App.js
@@ -4,7 +4,13 @@ function App() {
   return (
     <div className="game-container">
       <Card shape={"circle"} color={"blue"}/>
-      <Card shape={"circle"} color={"red"}/>
+      <Card shape={"square"} color={"red"}/>
+      <Card shape={"star"} color={"blue"}/>
+      <Card shape={"diamond"} color={"red"}/>
+      <Card shape={"heart"} color={"blue"}/>
+      <Card shape={"square"} color={"red"}/>
+      <Card shape={"circle"} color={"blue"}/>
+      <Card shape={"square"} color={"red"}/>
     </div>
   );
 }
--- src/components/Card.js
+++ src/components/Card.js
@@ -2,7 +2,19 @@ import React , {useState} from "react";
 import "./Card.css";
 import Icon from "./Icon";
 
+const SHAPES = ["circle", "triangle", "square", "star", "heart", "diamond", "pentagon", "octagon"];
+const COLORS = ["red", "orange", "yellow", "green", "blue", "indigo", "violet", "cyan"];
+
+const generateCard = ()=>{
+    return [SHAPES[(Math.floor(Math.random() * 7)) + 1], COLORS[(Math.floor(Math.random() * 7)) + 1]];
+}
+
 const Card = (props)=>{
+    console.log(generateCard());
+
+
+    
+   
 
     return (
         <div className="card">
