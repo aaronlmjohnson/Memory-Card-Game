9f4390ae26fd4a903201109143aa48ee9bdaad9a
Aaron <ajonaaronajon@yahoo.com>
2022-06-14 12:47:47 -0500

    add Deck.js

--- src/App.js
+++ src/App.js
@@ -4,14 +4,15 @@ import Card from './components/Card';
 function App() {
   return (
     <div className="game-container">
-      <Card shape={"circle"} color={"blue"}/>
-      <Card shape={"square"} color={"red"}/>
-      <Card shape={"star"} color={"blue"}/>
-      <Card shape={"diamond"} color={"red"}/>
-      <Card shape={"heart"} color={"blue"}/>
-      <Card shape={"square"} color={"red"}/>
-      <Card shape={"circle"} color={"blue"}/>
-      <Card shape={"square"} color={"red"}/>
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+
     </div>
   );
 }
---
+++ src/components/Deck.js
@@ -0,0 +1,18 @@
+import React , {useState} from "react";
+
+const Deck = (props)=>{
+
+    const SHAPES = ["circle", "triangle", "square", "star", "heart", "diamond", "pentagon", "hexagon"];
+    const COLORS = ["red", "orange", "yellow", "green", "blue", "indigo", "violet", "cyan"];
+    
+    const generateDeck = (size)=>{
+
+    }
+    return (
+        <div id="deck">
+            
+        </div>
+    )
+}
+
+export default Deck;
