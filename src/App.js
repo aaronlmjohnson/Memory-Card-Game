700699e3fb97ee3a6943bebd611af537be3ba2f0
Aaron <ajonaaronajon@yahoo.com>
2022-06-27 14:02:31 -0500

    changed generateCard to generateUniqueCard

--- src/App.js
+++ src/App.js
@@ -1,46 +1,23 @@
 import './App.css';
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import Deck from './components/Deck';
 import Scoreboard from './components/Scoreboard';
-import uniqid from 'uniqid';
+import useGenerateDeck from './components/useGenerateDeck';
 
 function App() {
-  const SHAPES = ["circle", "triangle", "square", "star", "heart", "diamond", "pentagon", "hexagon"];
-  const COLORS = ["red", "orange", "yellow", "green", "blue", "indigo", "violet", "cyan"];
+  const {deck, generateDeck} = useGenerateDeck();
 
-//need to generate deck here and pass to Deck component
-  const arrayEquals = (a, b) => {
-    return Array.isArray(a) &&
-      Array.isArray(b) &&
-      a.length === b.length &&
-      a.every((val, index) => val === b[index]);
-  }
+  useEffect(()=>{
+    generateDeck();
+  }, deck);
 
-  const isCardUnique = (deck, arr)=>{
-    if(arr === []) return false;
-    return !deck.map((card)=>{
-        return arrayEquals(card, arr);
-    }).includes(true);
-  }
-
-  const generateCard = ()=>{
-    return [SHAPES[(Math.floor(Math.random() * 8))], COLORS[(Math.floor(Math.random() * 8))], uniqid()];
-  }
-
-  const generateDeck = (size)=>{
-    const deck = [];
-    let card = [];
-    for(let i = 0; i < size; i++){
-        do{ card = generateCard(); }while(!isCardUnique(deck, card));
-        deck.push(card);
-    }
-    return deck;
-  }
-
+  console.log(deck);
+  
   return (
     <div className="game-container">
       <Scoreboard currentScore = {0} bestScore = {0} />
-      <Deck deck = {generateDeck(8)} isCardUnique = {isCardUnique}/>
+
+      {/* <Deck deck = {generateDeck(8)} isCardUnique = {isCardUnique}/> */}
     </div>
   );
 }
---
+++ src/components/useGenerateDeck.js
@@ -0,0 +1,47 @@
+import { useState } from "react";
+import uniqid from "uniqid";
+
+const useGenerateDeck = ()=>{
+    const SHAPES = ["circle", "triangle", "square", "star", "heart", "diamond", "pentagon", "hexagon"];
+    const COLORS = ["red", "orange", "yellow", "green", "blue", "indigo", "violet", "cyan"];
+    const greenTriangle = ["triangle", "green"];
+    const redCircle = ["circle" , "red"];
+
+    const [deck, setDeck] = useState([]);
+
+    const arrayEquals = (a, b) => {
+        return Array.isArray(a) &&
+          Array.isArray(b) &&
+          a.length === b.length &&
+          a.every((val, index) => val === b[index]);
+      }
+    
+      const isCardUnique = (deck, arr)=>{
+        if(arr === []) return false;
+        return !deck.map((card)=>{
+            return arrayEquals(card, arr);
+        }).includes(true);
+      }
+    
+      const generateUniqueCard = ()=>{
+        let card = [];
+
+        do{ 
+          card = [SHAPES[(Math.floor(Math.random() * 8))], COLORS[(Math.floor(Math.random() * 8))]];
+        }while(!isCardUnique(deck, card));
+
+        return [...card, uniqid()];
+      }
+      
+      const generateDeck = ()=>{
+        for(let i = 0; i < 8; i++)
+            setDeck( prevDeck => [...prevDeck, generateUniqueCard()]);
+      }
+      
+
+      return {deck, generateDeck};
+}
+
+export default useGenerateDeck;
+
+
