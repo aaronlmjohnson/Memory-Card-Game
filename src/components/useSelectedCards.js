22195b9b3188782ae663db1464f843a3ec3e6132
Aaron <ajonaaronajon@yahoo.com>
2022-06-28 13:52:20 -0500

    add cards to selectedCards on card click

--- src/components/useSelectedCards.js
+++ src/components/useSelectedCards.js
@@ -1,15 +1,43 @@
 import { useState } from "react";
-
+let score = 0,  bestScore = 0;
 const useSelectedCards = ()=>{
     const [selectedCards, setSelectedCards] = useState([]);
+    
 
     const addToSelected = (card)=> {
         
-        console.log(`add to ${card} selected`);
+        if(isCardUnique(selectedCards, card)){
+            setSelectedCards([...selectedCards, card]);
+            score += 1;
+        }else{
+            setSelectedCards([]);
+            bestScore = score > bestScore ? score : bestScore;
+            score = 0;
+        }
+        
+        console.log(`Score:${score}`);
+        console.log(`Best Score:${bestScore}\n\n`);
+        
     }
 
       return {selectedCards, addToSelected};
 }
+
+const arrayEquals = (a, b) => {
+    return Array.isArray(a) &&
+      Array.isArray(b) &&
+      a.length === b.length &&
+      a.every((val, index) => val === b[index]);
+  }
+
+  const isCardUnique = (deck, arr)=>{
+    if(arr === []) return false;
+    return !deck.map((card)=>{
+        return arrayEquals(card, arr);
+    }).includes(true);
+  }
+
+
 
 export default useSelectedCards;
 
